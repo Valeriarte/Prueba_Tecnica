538047439589d499d1ed9caeae511e0aea06f49f
Valeria <valesol4321@gmail.com>
2026-02-19 22:50:55 -0500

    [feat]: Se añade la funcionalidad de leer archivo de excel con pandas y se normalizan los datos.

---
+++ src/proceso.py
@@ -0,0 +1,28 @@
+import pathlib
+import pandas as pd
+import numpy as np
+import openpyxl
+from datetime import datetime
+
+# 1. Descarga del Archivo de Datos
+dir = pathlib.Path(__file__).resolve().parent.parent
+ruta_archivo = dir / "Insumos" / "datos_ventas.xlsx"
+
+
+# 2. Procesamiento de datos
+def leer_datos(ruta_archivo):
+    """ Funcion que lee los datos del archivo Excel."""
+    ruta = pathlib.Path(ruta_archivo)
+
+    if not ruta.exists():
+        raise FileNotFoundError(f"No se encontró el archivo: {ruta}")
+
+    return pd.read_excel(ruta)
+
+
+def limpiar_datos(df_ventas):
+    """ Funcion que normaliza los datos del excel, reemplazando valores nulos y cambiando el tipo de dato de fecha """
+
+    df_ventas["Total_Venta"] = df_ventas["Total_Venta"].fillna(df_ventas["Cantidad"] * df_ventas["Precio_Unitario"])
+    df_ventas["Fecha"] = pd.to_datetime(df_ventas["Fecha"])
+    return df_ventas
